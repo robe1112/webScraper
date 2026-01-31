05b96a18a4f75df88aaebe574ed62e5a1c8a7eec
Rob Evans <robevans1112@gmail.com>
2026-01-31 00:04:28 -0500

    Initial Commit

---
+++ webScraper/webScraperApp.swift
@@ -0,0 +1,17 @@
+//
+//  webScraperApp.swift
+//  webScraper
+//
+//  Created by Rob Evans on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct webScraperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ webScraperTests/webScraperTests.swift
@@ -0,0 +1,17 @@
+//
+//  webScraperTests.swift
+//  webScraperTests
+//
+//  Created by Rob Evans on 1/31/26.
+//
+
+import Testing
+@testable import webScraper
+
+struct webScraperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
